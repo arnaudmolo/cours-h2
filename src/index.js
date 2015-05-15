59a3163edcb9230af8a6a4de484deac157021b2f
Arnaud Pichon <arnaud@onesime.fr>
2015-05-15 16:50:06 +0200

    world

--- src/index.js
+++ src/index.js
@@ -1,59 +1,57 @@
 import d3 from 'd3';
 
-var outerWidth  = 500*2;
-var outerHeight = 250*2;
-var margin = { left: -50, top: 0, right: -50, bottom: 0 };
-
-var xColumn = "longitude";
-var yColumn = "latitude";
-var rColumn = "population";
-var peoplePerPixel = 1000000;
-
-var innerWidth  = outerWidth  - margin.left - margin.right;
-var innerHeight = outerHeight - margin.top  - margin.bottom;
-
-var svg = d3.select("body").append("svg")
-  .attr("width",  outerWidth)
-  .attr("height", outerHeight);
-
-var g = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var xScale = d3.scale.linear().range([0, innerWidth]);
-var yScale = d3.scale.linear().range([innerHeight, 0]);
-var rScale = d3.scale.sqrt();
-
-function render(data){
-
-  xScale.domain( d3.extent(data, function (d){ return d[xColumn]; }));
-  yScale.domain( d3.extent(data, function (d){ return d[yColumn]; }));
-  rScale.domain([0, d3.max(data, function (d){ return d[rColumn]; })]);
-
-  // Compute the size of the biggest circle as a function of peoplePerPixel.
-  var peopleMax = rScale.domain()[1];
-  var rMin = 0;
-  var rMax = Math.sqrt(peopleMax / (peoplePerPixel * Math.PI));
-  rScale.range([rMin, rMax]);
-
-  var circles = g.selectAll("circle").data(data);
-  circles.enter().append("circle");
-  circles
-    .attr("cx", function (d){ return xScale(d[xColumn]); })
-    .attr("cy", function (d){ return yScale(d[yColumn]); })
-    .attr("r",  function (d){ return rScale(d[rColumn]); });
-  circles.exit().remove();
-}
-
 function type(d){
-  d.latitude   = +d.latitude;
-  d.longitude  = +d.longitude;
-  d.population = +d.population;
-  return d;
+    Object.keys(d).forEach(function(key) {
+      const value = d[key];
+      if (!isNaN(+value)) {
+        d[key] = +value
+      }
+    });
+    return d;
 }
 
-d3.csv('world.csv', type, function(res) {
-  render(res);
+const outerWidth = 500;
+const outerHeight = 250;
+const margin = {
+  left: -50,
+  right: -50,
+  top: 0,
+  left: 0
+}
+
+const peoplePerPixel = 100000;
+
+const innerWidth = outerWidth - margin.left - margin.right;
+const innerHeight = outerHeight - margin.top - margin.bottom;
+
+const svg = d3.select('body').append('svg');
+
+svg
+  .attr('width', outerWidth)
+  .attr('height', outerHeight);
+
+const g = svg.append('g');
+
+g
+  .attr('transform', 'translate('+ margin.left +', ' + margin.top + ')')
+
+const xScale = d3.scale.linear().range([0, innerWidth]);
+const yScale = d3.scale.linear().range([innerHeight, 0]);
+const rScale = d3.scale.sqrt();
+
+function render(data) {
+
+  xScale
+    .domain(d3.extent(data, (d) => d.longitude));
+
+  yScale
+    .domain(d3.extent(data, (d) => d.latitude));
+
+  rScale
+    .domain([0, d3.max(data, (d) => d.population)]);
+
+}
+
+d3.csv('world.csv', type, function(data) {
+  render(data);
 })
-
-
-
