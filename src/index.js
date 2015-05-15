5bde834f62025d9dd7b2e9c1206b186f2642c6fb
Arnaud Pichon <arnaud@onesime.fr>
2015-05-15 11:54:40 +0200

    bon fichier world

--- src/index.js
+++ src/index.js
@@ -10,83 +10,77 @@ function type(d){
     return d;
 }
 
-const width = 900;
-const height = 900;
+const outerWidth = 500;
+const outerHeight = 250;
 
-const innerWidth = width - 30;
-const innerHeight = height - 30;
+const margin = {
+  left: -50,
+  right: -50,
+  top: 0,
+  bottom: 0
+}
 
-const radiusMin = 1;
-const radiusMax = 6;
+const peoplePerPixel = 100000;
 
-const xScale = d3.scale.linear()
-  .range([0, innerWidth]);
+const innerWith = outerHeight - margin.left - margin.right;
+const innerHeight = outerHeight - margin.top - margin.bottom;
 
-const yScale = d3.scale.linear()
-  .range([innerHeight, 0]);
+const svg = d3.select('body').append('svg');
 
-const rScale = d3.scale.linear()
-  .range([radiusMin, radiusMax]);
+svg
+  .attr('width', outerWidth)
+  .attr('height', outerHeight)
 
-const colorScale = d3.scale.category10();
+const g = svg.append('g');
 
-const svg = d3.select("body").append("svg")
-  .attr("width",  width)
-  .attr("height", height);
+g
+  .attr('transform', 'translate('+ margin.left +', '+ margin.top +')');
 
-  function render(data){
+const xScale = d3.scale.linear().range([0, innerWith]);
+const yScale = d3.scale.linear().range([innerHeight, 0]);
+const rScale = d3.scale.sqrt();
+const rMin   = 0;
 
-    const xExt = d3.extent(data, function (d){
-      return d.sepal_length;
-    });
+function render(data) {
 
-    const yExt = d3.extent(data, function (d){
-      return d.petal_length;
-    });
+  xScale.domain(d3.extent(data, (d) => d.longitude));
+  yScale.domain(d3.extent(data, (d) => d.latitude));
+  rScale.domain(d3.extent(data, (d) => d.population));
 
-    const rExt = d3.extent(data, function(d){
-      return d.sepal_width;
+  const peopleMax = rScale.domain()[1];
+  const rMax = Math.sqrt(peopleMax / (peoplePerPixel * Math.PI));
+
+  rScale.domain([rMin, rMax]);
+
+  console.log([rMin, rMax]);
+
+  const circles = g
+    .selectAll('circle')
+    .data(data);
+
+  circles
+    .exit()
+    .remove();
+
+  circles
+    .enter()
+    .append('circle');
+
+  circles
+    .attr('cx', (d) => xScale(d.longitude))
+    .attr('cy', (d) => {
+      console.log(d);
+      console.log(yScale(d.latitude));
+    })
+    .attr('r', (d) => {
+      return rScale(d.population);
     })
 
-    xScale.domain(xExt);
-
-    yScale.domain(yExt);
-
-    rScale.domain(rExt);
-
-    const g = svg.append('g');
-
-    const circles = g
-      .selectAll("circle")
-      .data(data);
-
-    g
-      .attr('transform', 'translate(15, 15)');
-
-    circles
-      .enter()
-      .append("circle")
-      .attr('stoke-width', 10)
-      .attr('stroke', 'gray');
-
-    circles
-      .attr("cx", function (d){
-        return xScale(d.sepal_length);
-      })
-      .attr("cy", function (d){
-        return yScale(d.petal_length);
-      })
-      .attr('fill', function(d) {
-        return colorScale(d.species);
-      })
-      .attr('r', function(d) {
-        return rScale(d.sepal_width)
-      })
-
-    circles.exit().remove();
-  }
+}
 
 d3.csv('world.csv', type, function(res) {
-  console.log(res);
   render(res);
-});
+})
+
+
+
